826b0824e22eb18c7645858f3ddaa47b60f8d4a6
粒粒橙 <i@llc.moe>
2021-05-31 23:07:38 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useRef } from 'react';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import SwiperInstance from 'swiper'
+import 'swiper/swiper.scss';
+import './App.sass';
 
 function App() {
+  const swiperRef = useRef<SwiperInstance>()
+
+  useEffect(() => {
+    const update = () => swiperRef.current?.update()
+    update()
+    window.addEventListener('resize', update)
+    return () => window.removeEventListener('resize', update)
+  }, [])
+
+  const onSwiperUpdate = useCallback((s: SwiperInstance) => {
+    const wrapper = s.wrapperEl
+
+    // remove existing progress bar
+    const existing = wrapper.getElementsByClassName('progress-bar')
+    while (existing[0]) {
+      existing[0].parentNode?.removeChild(existing[0]);
+    }
+    
+    // calculate progress bar length
+    const slides = wrapper.getElementsByClassName('swiper-slide') as HTMLCollectionOf<HTMLElement>
+    const firstSlide = slides[0]
+    const lastSlide = slides[slides.length - 1]
+    const totalWidth = wrapper.scrollWidth
+      + 2 * parseFloat(firstSlide.style.marginRight)
+      + 0.5 * firstSlide.scrollWidth
+      + 0.5 * lastSlide.scrollWidth
+    const left = parseFloat(firstSlide.style.marginRight) + 0.5 * firstSlide.scrollWidth 
+    let p = document.createElement('div')
+    p.classList.add('progress-bar')
+    p.style.width = `${totalWidth}px`
+    p.style.left = `-${left}px`
+
+    // insert into slides wrapper
+    wrapper.insertBefore(p, wrapper.firstElementChild)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Swiper
+        className="swiper"
+        spaceBetween={50}
+        slidesPerView={'auto'}
+        centeredSlides={true}
+        onSwiper={(swiper) => (swiperRef.current = swiper)}
+        onUpdate={onSwiperUpdate}
+      >
+        {[1,2,3,4,5,6,7].map(item => <SwiperSlide className="slide" key={item}>
+         {item}
+        </SwiperSlide>)}
+      </Swiper>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const doResize = (): void => {
+  const element = document.documentElement
+  const width = window.innerWidth || element.clientWidth
+  const height = window.innerHeight || element.clientHeight
+  let fontSize = Math.floor(Math.min(width / 7.5, 100) * 10000) / 10000
+  if (!/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+    fontSize = Math.floor(Math.min(fontSize, height / 12) * 10000) / 10000
+  }
+  element.style.fontSize = fontSize.toFixed(4) + 'px'
+  const realFontSize =
+    Math.floor(
+      parseFloat(
+        window.getComputedStyle(element).fontSize.replace('px', ''),
+      ) * 10000,
+    ) / 10000
+  if (fontSize !== realFontSize) {
+    element.style.fontSize =
+      (fontSize * (fontSize / realFontSize)).toFixed(4) + 'px'
+  }
+}
+
+window.addEventListener('resize', doResize)
+window.addEventListener('orientationchange', doResize)
+document.addEventListener('DOMContentLoaded', doResize)
+doResize()
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
